83cd7a2db8717c8a37cddb1d254cf61b11b51e0c
Wiktor6501 <95147172+Wiktor6501@users.noreply.github.com>
2023-10-15 19:36:36 +0100

    s
    s

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    setTimeout(redirect, 2000);
+  };
+  
+  function redirect() {
+    window.location.href = "https://youtube.com/";
+  }
+  
+  function redirectToAnotherPage() {
+    window.location.href = "https://youtube.com/";
+  }
